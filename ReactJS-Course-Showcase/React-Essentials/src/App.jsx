03085b10b290edd0e1e97bb8821c871f017168c1
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-12-09 16:44:36 +0330

    Deploying images dynamically

--- ReactJS-Course-Showcase/React-Essentials/src/App.jsx
+++ ReactJS-Course-Showcase/React-Essentials/src/App.jsx
@@ -1,16 +1,17 @@
+import reactImg from './assets/react-core-concepts.png'
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max + 1));
 }
-// console.log(genRandomInt(254));
 
 function Header() {
   const description = reactDescriptions[genRandomInt(2)];
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
 
       <h1>React Essentials</h1>
       <p>
