99f5a9c8b0348107aeb8d468067b6bb6f821575c
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-12-09 15:44:36 +0330

    Creating first component | Build a dynamic text

--- ReactJS-Course-Showcase/React-Essentials/src/App.jsx
+++ ReactJS-Course-Showcase/React-Essentials/src/App.jsx
@@ -1,14 +1,31 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max + 1));
+}
+// console.log(genRandomInt(254));
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
+
       <main>
         <h2>Time to get started!</h2>
       </main>
