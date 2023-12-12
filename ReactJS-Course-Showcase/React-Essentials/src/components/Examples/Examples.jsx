06c3dc9ff3ecc6da29d9c72f43a19c0231e1f919
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-12-12 16:34:47 +0330

    Setting Component types dynamically

--- ReactJS-Course-Showcase/React-Essentials/src/components/Examples/Examples.jsx
+++ ReactJS-Course-Showcase/React-Essentials/src/components/Examples/Examples.jsx
@@ -111,5 +111,21 @@ export default Examples;
 
 -> Working with JSX Slots(position-place):
 
+** if we want to add <TabButton ...>...</TabButton> to Tabs.jsx component, we will have the problem to handle "onClick" and "isSelected" because then we need to add Extra Props there(in Tabs.jsx) and adding these extra props makes Tabs.jsx less reuseable, THEREFORE -> we set an extra prop on Tabs.jsx and after that we can set this prop on Tabs component in Examples.jsx
+
+** BECAUSE -> JSX codes are in the end just regular values that can be used like values
+
+------------------------------------------------------
+
+-> Setting Component types dynamically:
+it must start with "Uppercase" character
+
+ButtonsContainer = {Section} -> custom component must be passed as a dynamic value
+
+ButtonsContainer = "menu" -> for built-in elements we shouldn't use {menu} because that would look for variable named menu and we don't have variable named menu in our code, THEREFORE we simply pass the identifier as a string""
+
+** ButtonsContainer = "menu" -> by setting this we're now able to dynamically set the "wrapper" that should be used around these "buttons" when we're using Tabs.jsx component
+
+==>> we can use this as default prop values -> therefore we omit this prop in this component and then try to give ButtonsContainer = "menu" as default value to Tabs.jsx component
 
 */
--- ReactJS-Course-Showcase/React-Essentials/src/components/Tabs.jsx
+++ ReactJS-Course-Showcase/React-Essentials/src/components/Tabs.jsx
@@ -1,7 +1,7 @@
-function Tabs({ children, buttons }) {
+function Tabs({ children, buttons, ButtonsContainer = 'menu' }) {
   return (
     <>
-      <menu>{buttons}</menu>
+      <ButtonsContainer>{buttons}</ButtonsContainer>
 
       {children}
     </>
@@ -10,13 +10,26 @@ function Tabs({ children, buttons }) {
 
 export default Tabs;
 
+/* Extra information:
 
-/* Working with JSX Slots(position-place):
+-> Working with JSX Slots(position-place):
 
--> main content slot-> is set with help of "children"
+ main content slot-> is set with help of "children"
 
--> buttons slot-> is set with help of our "custom buttons prop"
+ buttons slot-> is set with help of our "custom buttons prop"
 
 ** we could set more "slot" if our component would needed extra slots for extra JSX content that should be set
 
+-----------------------------------------------------------
+
+-> Setting Component types dynamically:
+it must start with "Uppercase" character
+
+ButtonsContainer = "menu" -> we passed "menu" as a identifier and then using this identifier inside of a component to "Dynamically Render Different HTML Elements"
+
+==> Even we can use this prop as default prop values like -> 
+ButtonsContainer = 'menu' just in this component
+
+for built-in elements -> ButtonsContainer = 'menu' 
+for custom component -> ButtonsContainer = Section <- we would have to import this Section component here as well
 */
