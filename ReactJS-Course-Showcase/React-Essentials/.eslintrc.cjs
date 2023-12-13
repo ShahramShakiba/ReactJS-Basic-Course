2efb22203874240bc758867362bfd0732790a950
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-12-13 12:38:33 +0330

    Fix .eslintrc error missing prop validation

--- ReactJS-Course-Showcase/React-Essentials/.eslintrc.cjs
+++ ReactJS-Course-Showcase/React-Essentials/.eslintrc.cjs
@@ -12,9 +12,10 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'react/prop-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
