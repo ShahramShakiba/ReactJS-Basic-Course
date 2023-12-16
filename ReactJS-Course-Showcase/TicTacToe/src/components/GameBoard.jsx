14c0bf2ae61e18818ef30558c3bbe8ccac8d6c80
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-12-16 19:23:34 +0330

    Updating Object State Immutably

--- ReactJS-Course-Showcase/TicTacToe/src/components/GameBoard.jsx
+++ ReactJS-Course-Showcase/TicTacToe/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,31 @@ const initialGameBoard = [
 ];
 
 function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  //=> update the GameBoard based on the Previous state
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+
+      updatedBoard[rowIndex][colIndex] = 'X';
+
+      return updatedBoard;
+    });
+  };
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
@@ -39,5 +58,32 @@ export default GameBoard;
 -> to output these inner arrays(null) -> we need another list (ol) because we need a grid
 -> <button></button> -> to output the" user symbols (X or O or null)
 
+------------------------------------------------------------
+
+* in "handleSelectSquare" function
+? rowIndex -> on which row index we have; which inner array contains the field that should be updated
+
+? colIndex -> which exact field was clicked; which null should be replaced with an "X" or an "O"
+
+❌❌❌ NOT RECOMMENDED ❌❌❌
+? prevGameBoard[rowIndex][colIndex] = "X"; 
+[rowIndex]-> select on of 3 arrays
+[colIndex]-> select on of the elements inside of that inner arrays
+
+❌ -> if your state is an "Object" or "Array" you're dealing with a ->"ReferencE-Value"<- in JavaScript; 
+-> therefore if you would be updating like this you would be updating the "old value in-memory" immediately, even before  this scheduled state update was executed by React
+=>> this can lead to bugs and side effects
+
+✅✅✅ RECOMMENDED ✅✅✅
+const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+
+-> map()-> since we're dealing with some nested arrays those should be copied as well before update anything, we can achieve this by calling map
+
+-> innerArray-> then we got inner-arrays inside of nested array, therefore we spreads the elements of that inner-array
+
+
+? -> which symbol should be placed
 
 */
