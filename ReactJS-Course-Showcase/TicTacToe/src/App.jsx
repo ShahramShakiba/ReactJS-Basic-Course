4bf05346a62b0108ef80850ba5b6cc4c29ceea5f
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-12-16 15:23:47 +0330

    Make Editing Button dynamic

--- ReactJS-Course-Showcase/TicTacToe/src/App.jsx
+++ ReactJS-Course-Showcase/TicTacToe/src/App.jsx
@@ -17,3 +17,17 @@ function App() {
 }
 
 export default App;
+
+/* EXTRA Information
+
+? Reusing Component:
+whenever you are using or reusing a component, React will basically create a new isolated instance, for instance:
+
+-> <Player name="Player 1" symbol="X" />
+-> <Player name="Player 2" symbol="O" />
+These two component work totally isolated from each other
+
+* important feature in React
+Having this isolation allows you to build super complex reusable components
+
+*/
--- ReactJS-Course-Showcase/TicTacToe/src/components/Player.jsx
+++ ReactJS-Course-Showcase/TicTacToe/src/components/Player.jsx
@@ -4,12 +4,16 @@ function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEdiClick = () => {
-    setIsEditing(true);
+    setIsEditing(!isEditing);
   };
 
   let playerName = <span className="player-name">{name}</span>;
+  // let btnCaption = 'Edit';
+
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
+
+    // btnCaption = 'Save';
   }
 
   return (
@@ -20,7 +24,8 @@ function Player({ name, symbol }) {
         <span className="player-symbol">{symbol}</span>
       </span>
 
-      <button onClick={handleEdiClick}>Edit</button>
+      {/* <button onClick={handleEdiClick}>{btnCaption}</button> */}
+      <button onClick={handleEdiClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   );
 }
