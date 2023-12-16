d951d4f928b098208aefec95b0abbce28171155f
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-12-16 15:54:22 +0330

    Updating State based on Previous State

--- ReactJS-Course-Showcase/TicTacToe/src/components/Player.jsx
+++ ReactJS-Course-Showcase/TicTacToe/src/components/Player.jsx
@@ -4,7 +4,7 @@ function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEdiClick = () => {
-    setIsEditing(!isEditing);
+    setIsEditing((editing) => !editing);
   };
 
   let playerName = <span className="player-name">{name}</span>;
@@ -31,3 +31,23 @@ function Player({ name, symbol }) {
 }
 
 export default Player;
+
+/* EXTRA information
+
+---> If Your Updating State Based On Previous State Value <---
+
+*❌ setIsEditing(!isEditing) => schedule a state-update to true, but does not immediately change that state ❌
+if your New State Depends on Your Previous State value 
+-> you "-> should NOT <-" update the state like this
+
+?-> React is scheduling those state updating functions, so this state update here "is not performed instantly" instead, it's scheduled by React to be performed in the future; the future will probably be in 1 or 2 milliseconds, but it's not "instant"
+
+*✅ setIsEditing(wasEditing => !wadEditing) ✅
+instead "Pass a Function" to your state updating function
+-> this function will automatically be called by React and will receive the Guaranteed Latest State Value.
+
+-> this function will get the current-value as an input
+
+? You simply have a guarantee by React that you'll always be working with the latest available state value.
+
+*/
