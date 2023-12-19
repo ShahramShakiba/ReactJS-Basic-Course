1205af1e7493a330726d5b712a583cc53a3c78f5
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-12-19 16:30:39 +0330

    Update the Players data

--- ReactJS-Course-Showcase/TicTacToe/src/App.jsx
+++ ReactJS-Course-Showcase/TicTacToe/src/App.jsx
@@ -23,6 +23,10 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
+  const [players, setPlayers] = useState({
+    X: 'Player 1',
+    O: 'Player 2',
+  });
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
@@ -74,6 +78,15 @@ function App() {
 
   function handleRestart() {
     setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName,
+      };
+    });
   }
 
   return (
