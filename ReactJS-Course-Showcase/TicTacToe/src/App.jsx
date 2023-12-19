8f08587b62f14d61ed91a06784b49cc1bbedce16
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-12-19 13:59:43 +0330

    Lifting Computed Values of GameBoard-Component Up

--- ReactJS-Course-Showcase/TicTacToe/src/App.jsx
+++ ReactJS-Course-Showcase/TicTacToe/src/App.jsx
@@ -5,6 +5,12 @@ import GameBoard from './components/GameBoard';
 import Log from './components/Log';
 import WINNING_COMBINATIONS from './winning-combination';
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
@@ -18,6 +24,20 @@ function App() {
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+
+    for(const combination of WINNING_COMBINATIONS) {
+      const firstSquareSymbol
+      const secondSquareSymbol
+      const thirdSquareSymbol
+    }
+  }
 
   //-> executed whenever we wanna switch-turns in "GameBoard"
   const handleSelectSquare = (rowIndex, colIndex) => {
@@ -53,7 +73,7 @@ function App() {
           />
         </ol>
 
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
 
       <Log turns={gameTurns} />
--- ReactJS-Course-Showcase/TicTacToe/src/components/GameBoard.jsx
+++ ReactJS-Course-Showcase/TicTacToe/src/components/GameBoard.jsx
@@ -1,22 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
+function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
