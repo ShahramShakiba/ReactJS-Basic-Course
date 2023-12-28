d77ffccdcccc469c35b1948cce55d36c92d4dce9
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-12-28 11:42:53 +0330

    Implementing Header component

--- ReactJS-Course-Showcase/Investment-Calculator/src/App.jsx
+++ ReactJS-Course-Showcase/Investment-Calculator/src/App.jsx
@@ -1,7 +1,7 @@
+import Header from './components/Header.jsx';
+
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+  return <Header />;
 }
 
-export default App
+export default App;
--- ReactJS-Course-Showcase/Investment-Calculator/src/util/investment.js
+++ ReactJS-Course-Showcase/Investment-Calculator/src/util/investment.js
@@ -1,9 +1,12 @@
-// This function expects a JS object as an argument
-// The object should contain the following properties
-// - initialInvestment: The initial investment amount
-// - annualInvestment: The amount invested every year
-// - expectedReturn: The expected (annual) rate of return
-// - duration: The investment duration (time frame)
+/* 
+-> This function expects a JS object as an argument
+-> The object should contain the following properties :
+
+ ?- initialInvestment: The initial investment amount
+ ?- annualInvestment: The amount invested every year
+ ?- expectedReturn: The expected (annual) rate of return
+ ?- duration: The investment duration (time frame)
+*/
 export function calculateInvestmentResults({
   initialInvestment,
   annualInvestment,
@@ -15,7 +18,7 @@ export function calculateInvestmentResults({
 
   for (let i = 0; i < duration; i++) {
     const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-    
+
     investmentValue += interestEarnedInYear + annualInvestment;
     annualData.push({
       year: i + 1, // year identifier
@@ -28,9 +31,12 @@ export function calculateInvestmentResults({
   return annualData;
 }
 
-// The browser-provided Intl API is used to prepare a formatter object
-// This object offers a "format()" method that can be used to format numbers as currency
-// Example Usage: formatter.format(1000) => yields "$1,000"
+/* 
+* The browser-provided Intl API is used to prepare a formatter object
+=> This object offers a "format()" method that can be used to format numbers as currency
+
+ => Example Usage: formatter.format(1000) => yields "$1,000"
+*/
 export const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
