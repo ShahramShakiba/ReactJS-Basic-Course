9be3b35c74abbb551f95ccad2ccd6074632f1837
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-12-30 11:43:18 +0330

    Outputting Results in a List

--- ReactJS-Course-Showcase/Investment-Calculator/src/components/Results.jsx
+++ ReactJS-Course-Showcase/Investment-Calculator/src/components/Results.jsx
@@ -1,9 +1,46 @@
-import { calculateInvestmentResults } from '../util/investment';
+import { calculateInvestmentResults, formatter } from '../util/investment';
 
 export default function Results({ input }) {
   const resultData = calculateInvestmentResults(input);
+  const initialInvestment =
+    resultData[0].valueEndOfYear - // result for the first Year
+    resultData[0].interest -
+    resultData[0].annualInvestment;
 
-  return <div>Results</div>;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {resultData.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+
+          const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
 /* Derive Our Results
