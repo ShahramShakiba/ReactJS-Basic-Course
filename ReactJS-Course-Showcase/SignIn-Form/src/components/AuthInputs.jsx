9c4b979c6a1eab41230dcd3e4f91e4c88bf81c45
Shahram Shakiba <ShahramShakiba88@gmail.com>
2024-01-02 15:37:24 +0330

    Migrating the Demo App to Tailwind CSS

--- ReactJS-Course-Showcase/SignIn-Form/src/components/AuthInputs.jsx
+++ ReactJS-Course-Showcase/SignIn-Form/src/components/AuthInputs.jsx
@@ -1,15 +1,7 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
 
 import Button from './Button.jsx';
 import Input from './Input.jsx';
-
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -32,8 +24,11 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800"
+    >
+      <div className="flex flex-col gap-2 mb-6">
         <Input
           label="email"
           type="email"
@@ -49,10 +44,10 @@ export default function AuthInputs() {
             handleInputChange('password', event.target.value)
           }
         />
-      </ControlContainer>
+      </div>
 
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
@@ -180,4 +175,68 @@ FOR EXAMPLE in "Label-component" :
 -> no clear separation between React and CSS code
 -> you end up with many relatively small "wrapper" components
 
+*/
+
+/* Styled-Component CODES
+?import { styled } from 'styled-components';
+
+?import Button from './Button.jsx';
+?import Input from './Input.jsx';
+
+?const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+?export default function AuthInputs() {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  ?function handleInputChange(identifier, value) {
+    if (identifier === 'email') {
+      setEnteredEmail(value);
+    } else {
+      setEnteredPassword(value);
+    }
+  }
+
+  ?function handleLogin() {
+    setSubmitted(true);
+  }
+
+  ?const emailNotValid = submitted && !enteredEmail.includes('@');
+  ?const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+
+  ?return (
+    <div id="auth-inputs">
+      <ControlContainer>
+        <Input
+          label="email"
+          type="email"
+          invalid={emailNotValid}
+          onChange={(event) => handleInputChange('email', event.target.value)}
+        />
+
+        <Input
+          label="password"
+          type="password"
+          invalid={passwordNotValid}
+          onChange={(event) =>
+            handleInputChange('password', event.target.value)
+          }
+        />
+      </ControlContainer>
+
+      <div className="actions">
+        <button type="button" className="text-button">
+          Create a new account
+        </button>
+        <Button onClick={handleLogin}>Sign In</Button>
+      </div>
+    </div>
+  );
+}
 */
