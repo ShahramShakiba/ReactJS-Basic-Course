80a9ea667ae567cf43359477669eec377a5d1e4f
Shahram Shakiba <ShahramShakiba88@gmail.com>
2024-01-02 14:05:04 +0330

    Style Header with Tailwind CSS

--- ReactJS-Course-Showcase/SignIn-Form/src/components/Header.jsx
+++ ReactJS-Course-Showcase/SignIn-Form/src/components/Header.jsx
@@ -1,60 +1,18 @@
 import logo from '../assets/logo.png';
-// import classes from './Header.module.css';
-import { styled } from 'styled-components';
-
-const StyledHeader = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 2rem;
-  margin-bottom: 2rem;
-
-  & img {
-    object-fit: contain;
-    margin-bottom: 2rem;
-    width: 11rem;
-    height: 11rem;
-  }
-
-  & h1 {
-    font-size: 1.5rem;
-    font-weight: 600;
-    letter-spacing: 0.4em;
-    text-align: center;
-    text-transform: uppercase;
-    color: #9a3412;
-    font-family: 'Pacifico', cursive;
-    margin: 0;
-  }
-
-  & p {
-    text-align: center;
-    color: #a39191;
-    margin: 0;
-  }
-
-  @media (min-width: 768px) {
-    /* 
-    & {
-      margin-bottom: 4rem;
-    } 
-    */
-    margin-bottom: 4rem;
-
-    & h1 {
-      font-size: 2.25rem;
-    }
-  }
-`;
 
 export default function Header() {
   return (
-    <StyledHeader>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
-    </StyledHeader>
+    <header className="flex flex-col items-center mt-8 mb-16">
+      <img
+        src={logo}
+        alt="A canvas"
+        className="mb-8 w-44 h-44 object-contain"
+      />
+      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
+        ReactArt
+      </h1>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
+    </header>
   );
 }
 
@@ -136,5 +94,66 @@ export default function Header() {
 => & img {
     .....
   }  => with this rule( & ) here should affect any image elements that are inside of this header
+
+*/
+
+/* Styled-Component CODE
+? import { styled } from 'styled-components';
+
+?const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+
+  & img {
+    object-fit: contain;
+    margin-bottom: 2rem;
+    width: 11rem;
+    height: 11rem;
+  }
+
+  & h1 {
+    font-size: 1.5rem;
+    font-weight: 600;
+    letter-spacing: 0.4em;
+    text-align: center;
+    text-transform: uppercase;
+    color: #9a3412;
+    font-family: 'Pacifico', cursive;
+    margin: 0;
+  }
+
+  & p {
+    text-align: center;
+    color: #a39191;
+    margin: 0;
+  }
+
+  @media (min-width: 768px) {
+    
+    & {
+      margin-bottom: 4rem;
+    } 
+    
+OR just this -> margin-bottom: 4rem;
+
+    & h1 {
+      font-size: 2.25rem;
+    }
+  }
+`;
+
+?export default function Header() {
+  return (
+    <StyledHeader>
+      <img src={logo} alt="A canvas" />
+      <h1>ReactArt</h1>
+      <p>A community of artists and art-lovers.</p>
+    </StyledHeader>
+  );
+}
 
 */
--- ReactJS-Course-Showcase/SignIn-Form/tailwind.config.js
+++ ReactJS-Course-Showcase/SignIn-Form/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      // custom font
+      fontFamily: {
+        title: ['"Pacifico"', 'cursive'],
+      },
+    },
   },
   plugins: [],
 };
+
+/* ['"Pacifico"']
+-> the double quotes are only needed for this special font, which is imported from Google Fonts and added as a link in index.html
+
+-> this is how it's required by Tailwind to work
+*/
