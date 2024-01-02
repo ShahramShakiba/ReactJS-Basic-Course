41b75d0520372b7a8c2b978bcf8a0b30773caf4e
Shahram Shakiba <ShahramShakiba88@gmail.com>
2024-01-02 14:14:48 +0330

    Tailwind Media Queries | Pseudo Selectors

--- ReactJS-Course-Showcase/SignIn-Form/src/components/Button.jsx
+++ ReactJS-Course-Showcase/SignIn-Form/src/components/Button.jsx
@@ -1,6 +1,18 @@
-import { styled } from 'styled-components';
+export default function Button({ children, ...props }) {
+  return (
+    <button
+      className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
 
-const Button = styled.button`
+/* Styled-Component CODES
+? import { styled } from 'styled-components';
+
+?const Button = styled.button`
   padding: 1rem 2rem;
   font-weight: 600;
   text-transform: uppercase;
@@ -15,4 +27,5 @@ const Button = styled.button`
   }
 `;
 
-export default Button;
+?export default Button;
+*/
--- ReactJS-Course-Showcase/SignIn-Form/src/components/Header.jsx
+++ ReactJS-Course-Showcase/SignIn-Form/src/components/Header.jsx
@@ -2,13 +2,13 @@ import logo from '../assets/logo.png';
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mt-8 mb-16">
+    <header className="flex flex-col items-center mt-8 mb-8 md:mb-16">
       <img
         src={logo}
         alt="A canvas"
         className="mb-8 w-44 h-44 object-contain"
       />
-      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
+      <h1 className="text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
         ReactArt
       </h1>
       <p className="text-stone-500">A community of artists and art-lovers.</p>
