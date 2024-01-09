8e3c8a349861caab5b3515eed2e01d763a3223e3
Shahram Shakiba <ShahramShakiba88@gmail.com>
2024-01-09 10:53:37 +0330

    Use Styled-Component on Button Component

--- ReactJS-Course-Showcase/CourseList-Todo/src/components/UI/Button/Button.js
+++ ReactJS-Course-Showcase/CourseList-Todo/src/components/UI/Button/Button.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import styled from 'styled-components';
 
+// tagged template literal
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
+/*
+* the Button also by default applies all the "props" you might be passing to your own button-component
+*/
+
+export default Button;
+
+/* 
+import React from 'react';
 import './Button.css';
 
 const Button = props => {
@@ -9,5 +38,4 @@ const Button = props => {
     </button>
   );
 };
-
-export default Button;
+*/
