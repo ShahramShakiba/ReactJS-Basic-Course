8bf550e437bbdbee288565bead10a3dac49bd606
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-01-11 22:26:45 +0330

    Update

--- ReactJS-Course-Showcase/Final-CountDown/src/components/Player.jsx
+++ ReactJS-Course-Showcase/Final-CountDown/src/components/Player.jsx
@@ -1,5 +1,38 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
+export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setSubmitted(false);
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
+
+      <p>
+        <input
+          ref={playerName}
+          type="text"
+          onChange={handleChange}
+          value={enteredPlayerName}
+        />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
+
+/* Step 1
 export default function Player() {
   const [enteredPlayerName, setEnteredPlayerName] = useState('');
   const [submitted, setSubmitted] = useState(false);
@@ -24,3 +57,5 @@ export default function Player() {
     </section>
   );
 }
+
+*/
