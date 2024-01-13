b49ba0f7c66397d9f558ee3d70429169ac5f9cf4
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-01-13 11:16:14 +0330

    State VS Refs

--- ReactJS-Course-Showcase/Final-CountDown/src/components/Player.jsx
+++ ReactJS-Course-Showcase/Final-CountDown/src/components/Player.jsx
@@ -6,11 +6,14 @@ export default function Player() {
 
   function handleClick() {
     setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = '';
   }
 
   return (
     <section id="player">
-      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
+      <h2>
+        Welcome {enteredPlayerName ? enteredPlayerName : 'unknown entity'}
+      </h2>
 
       <p>
         <input ref={playerName} type="text" />
@@ -59,4 +62,19 @@ for example; where you wanna rea a value from an input field, it can save you a 
 
 You can connect "refs" to JSX elements with a special prop "ref={}" which like the key-prop is a special prop
 
+* for all refs values, you have to access a "current" property
+-> and then in this current-property that the actual ref-value, so in this case,
+the connected "input" will be stored -> now I can access all the methods and properties that are exposed by that input HTML-element
+
+
+? whenever a ref changes, the component function does not re-execute.
+
+! State
+1. Causes component re-evaluation(re-execution) when changed
+2. Should be used for values that are directly reflected in the UI
+3. Should not be used for "behind the scenes" values that have no direct UI impact
+
+! Refs
+1.Do not cause component re-evaluation when changed
+2. Can be used to gain direct DOM element access(great for reading values or accessing certain browser APIs)
 */
