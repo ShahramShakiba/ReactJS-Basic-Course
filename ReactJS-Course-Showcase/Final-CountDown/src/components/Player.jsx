79b14b706784cfc032a7662c88ff0c7d72cb145c
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-01-13 10:27:46 +0330

    Accessing HTML Elements via Refs

--- ReactJS-Course-Showcase/Final-CountDown/src/components/Player.jsx
+++ ReactJS-Course-Showcase/Final-CountDown/src/components/Player.jsx
@@ -2,37 +2,25 @@ import { useRef, useState } from 'react';
 
 export default function Player() {
   const playerName = useRef();
-
   const [enteredPlayerName, setEnteredPlayerName] = useState('');
-  const [submitted, setSubmitted] = useState(false);
-
-  function handleChange(event) {
-    setSubmitted(false);
-    setEnteredPlayerName(event.target.value);
-  }
 
   function handleClick() {
-    setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
 
       <p>
-        <input
-          ref={playerName}
-          type="text"
-          onChange={handleChange}
-          value={enteredPlayerName}
-        />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
 }
 
-/* Step 1
+/*  *Step 1*
 export default function Player() {
   const [enteredPlayerName, setEnteredPlayerName] = useState('');
   const [submitted, setSubmitted] = useState(false);
@@ -57,5 +45,18 @@ export default function Player() {
     </section>
   );
 }
+
+*/
+
+/* useRef()
+* useRef is a React Hook that lets you reference a value that’s not needed for rendering.
+
+for example; where you wanna rea a value from an input field, it can save you a lot of code and lead to leaner Components.
+
+? must be called inside of a Component-function
+
+? Here, I create a "ref" to get the value that will be entered into this input field 
+
+You can connect "refs" to JSX elements with a special prop "ref={}" which like the key-prop is a special prop
 
 */
