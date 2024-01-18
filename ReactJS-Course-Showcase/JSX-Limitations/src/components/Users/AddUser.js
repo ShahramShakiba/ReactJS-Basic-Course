6332b6fcdb981a36c010933561d4e290557a1c4e
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-01-18 12:06:47 +0330

    JSX Limitations

--- ReactJS-Course-Showcase/JSX-Limitations/src/components/Users/AddUser.js
+++ ReactJS-Course-Showcase/JSX-Limitations/src/components/Users/AddUser.js
@@ -52,6 +52,7 @@ const AddUser = (props) => {
           onConfirm={errorHandler}
         />
       )}
+
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
@@ -61,6 +62,7 @@ const AddUser = (props) => {
             value={enteredUsername}
             onChange={usernameChangeHandler}
           />
+
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
@@ -68,6 +70,7 @@ const AddUser = (props) => {
             value={enteredAge}
             onChange={ageChangeHandler}
           />
+
           <Button type="submit">Add User</Button>
         </form>
       </Card>
@@ -76,3 +79,70 @@ const AddUser = (props) => {
 };
 
 export default AddUser;
+
+/* 01. One limitations of JSX {One JSX element to return}
+* You can't return more than one "root" JSX element (you also can't store more than one "root" JSX element in a variable).
+
+? Solution:
+01. wrapping elements with a "div" or any other element, even a custom Component
+
+02. using an array []
+return [
+    error && (
+      <ErrorModal
+        key="error-modal"
+        title={error.title}
+        message={error.message}
+        onConfirm={errorHandler}
+      />
+    ),
+
+    <Card key="add-user-card" className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
+
+        <label htmlFor="age">Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
+
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>,
+  ];
+
+  ? This has a little gotcha: 
+    we can return an array because React is able to work with arrays of JSX elements, BUT
+        * whenever you're working with an array of JSX elements React wants a "key" on every element
+
+        * we could add the key prop with our own invented well use 
+
+    * this way is a bit "cumbersome" 
+*/
+
+/* "div soup"
+<div>
+  <div>
+    <div>
+      <div>
+        <h2>Some content -yeah, this can really happen</h2>
+      </div>
+    </div>
+  </div>
+</div>
+
+? in bigger apps, you can easily end up with tons of unnecessary <div>s (or other elements) which add "no semantic meaning or structure" to the page but "are only there because of React's/JSX requirement".
+
+? that also makes your application slower because the browser needs to render all those elements and React needs to check all those elements or at least some of those elements if content needs to change
+
+* Rendering unnecessary content is generally never a good idea in programming!
+*/
