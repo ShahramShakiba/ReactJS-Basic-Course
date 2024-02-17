96053e93bea3533ddfab5cb01611a93f24d23e91
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-02-17 12:52:16 +0330

    🛠️ Adding the New Project Component

--- ReactJS-Course-Showcase/Project-Management/src/App.jsx
+++ ReactJS-Course-Showcase/Project-Management/src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from './components/NewProject';
 import ProjectSideBar from './components/ProjectSideBar';
 
 export default function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSideBar />
+      <NewProject />
     </main>
   );
 }
---
+++ ReactJS-Course-Showcase/Project-Management/src/components/NewProject.jsx
@@ -0,0 +1,33 @@
+export default function NewProject() {
+  return (
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li>
+          <button className="text-stone-600 hover:text-rose-500 hover:border-b hover:border-rose-600 transition-color ease-in-out duration-300">
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button className="px-6 py-2 rounded-md bg-amber-300 text-stone-700 font-semibold hover:bg-amber-400 transition-all ease-in duration-150 shadow-md">
+            Save
+          </button>
+        </li>
+      </menu>
+
+      <div>
+        <p className="flex flex-col gap-1 my-4">
+          <label>Title</label>
+          <input />
+        </p>
+        <p className="flex flex-col gap-1 my-4">
+          <label>Description</label>
+          <textarea />
+        </p>
+        <p className="flex flex-col gap-1 my-4">
+          <label>Due Date</label>
+          <input />
+        </p>
+      </div>
+    </div>
+  );
+}
