b8f10caf3da63746a4221e0f56911d48875b186d
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-03-10 22:31:07 +0330

    🚨 Working on a More Complex Reducer Logic | Updating Items in the Cart

--- ReactJS-Course-Showcase/Food-Order-App/src/context/CartProvider.js
+++ ReactJS-Course-Showcase/Food-Order-App/src/context/CartProvider.js
@@ -8,9 +8,26 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD_ITEM') {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
 
     return {
       items: updatedItems,
